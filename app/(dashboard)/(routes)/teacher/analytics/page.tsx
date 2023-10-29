503796341c1d01507589e0228c7285a1595d9144
unknown <waelmanai96@gmail.com>
2023-10-29 16:56:58 +0000

    Adding dynamic layouts

---
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -0,0 +1,9 @@
+const AnalyticsPage = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default AnalyticsPage;
--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Compass, Layout } from "lucide-react";
-import { SidebarItem } from "./sidebar-item"
+import { Compass, Layout, List, BarChart } from "lucide-react";
+import { SidebarItem } from "./sidebar-item";
+import { usePathname } from "next/navigation";
 
 const guestRoutes = [
     {
@@ -14,10 +15,25 @@ const guestRoutes = [
         label: "Browse",
         href: "/search",
     },
-]
+];
+
+const teacherRoutes = [
+    {
+        icon: List,
+        label: "Courses",
+        href: "/teacher/courses",
+    },
+    {
+        icon: BarChart,
+        label: "Analytics",
+        href: "/teacher/analytics",
+    },
+];
 
 export const SidebarRoutes = () => {
-    const routes = guestRoutes;
+    const pathname = usePathname();
+    const isTeacherPage = pathname?.includes("/teacher");
+    const routes = isTeacherPage ? teacherRoutes : guestRoutes;
 
     return (
         <div className="flex flex-col w-full">
