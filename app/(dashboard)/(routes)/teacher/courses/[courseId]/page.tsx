fa3b58a14fe250206f86629f90e9af00147405d4
unknown <waelmanai96@gmail.com>
2023-11-02 19:11:06 +0100

    Course Description Form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -4,6 +4,7 @@ import { auth } from "@clerk/nextjs";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
 import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/description-form";
 
 const CourseIdPage = async ({
     params
@@ -61,6 +62,10 @@ const CourseIdPage = async ({
                         </h2>
                     </div>
                     <TitleForm 
+                        initialData={course}
+                        courseId={course.id}
+                    />
+                    <DescriptionForm 
                         initialData={course}
                         courseId={course.id}
                     />
