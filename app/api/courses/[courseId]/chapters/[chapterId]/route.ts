2ade08df6127770151a3fe16e95f9dd3dd0b89ce
unknown <waelmanai96@gmail.com>
2023-11-08 00:46:21 +0100

    Chapter publish and unpublish actions

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -8,6 +8,89 @@ const { Video } = new Mux(
     process.env.MUX_TOKEN_SECRET!,
 );
 
+
+export async function DELETE(
+    req: Request,
+    { params }: { params: {courseId: string; chapterId: string} }
+) {
+    try {
+        const { userId } = auth();
+
+        if(!userId){
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        const courseOwner = await db.course.findUnique({
+            where: {
+                id: params.courseId,
+                userId: userId,
+            }
+        })
+
+        if (!courseOwner) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        const chapter = await db.chapter.findUnique({
+            where: {
+                id: params.chapterId,
+                courseId: params.courseId
+            },
+        });
+
+        if (!chapter) {
+            return new NextResponse("Not found", { status: 404 });
+        }
+
+        if (chapter.videoUrl) {
+            const existingMuxData = await db.muxData.findFirst({
+                where: {
+                    chapterId: params.chapterId,
+                }
+            });
+
+            if(existingMuxData) {
+                await Video.Assets.del(existingMuxData.assetId);
+                await db.muxData.delete({
+                    where: {
+                        id: existingMuxData.id,
+                    }
+                });
+            }
+        }
+
+        const deleteChapter = await db.chapter.delete({
+            where: {
+                id: params.chapterId,
+            }
+        });
+
+        const publishedChaptersInCourse = await db.chapter.findMany({
+            where: {
+                courseId: params.chapterId,
+                isPublished: true,
+            }
+        });
+
+        if (!publishedChaptersInCourse) {
+            await db.course.update({
+                where: {
+                    id: params.courseId,
+                },
+                data: {
+                    isPublished: false,
+                }
+            });
+        }
+
+        return NextResponse.json(deleteChapter);
+
+    } catch (error) {
+        console.log("[CHAPTERS_ID_DELETE]", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
+
 export async function PATCH(
     req: Request,
     { params }: { params: {courseId: string; chapterId: string} }
---
+++ components/modals/confirm-modal.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { 
+    AlertDialog,
+    AlertDialogHeader,
+    AlertDialogFooter,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogCancel,
+    AlertDialogTitle,
+    AlertDialogAction,
+    AlertDialogTrigger
+} from "@/components/ui/alert-dialog";
+
+interface ConfirmModalProps {
+    children: React.ReactNode;
+    onConfirm: () => void;
+};
+
+export const ConfirmModal = ({
+    children,
+    onConfirm
+}: ConfirmModalProps) => {
+    return (
+
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                {children}
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        This action cannot be undone.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction onClick={onConfirm}>
+                        Continue
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    )
+}
