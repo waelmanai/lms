3e47d8615c5d2d6019bd472590c3ba2fd429ad1d
unknown <waelmanai96@gmail.com>
2023-11-11 01:48:16 +0100

    Course Grid &  Card UI & Get Progress Action & Get Course Action

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,13 +1,38 @@
 import { db } from "@/lib/db";
 import { Categories } from "./_components/categories";
 import { SearchInput } from "@/components/search-input";
+import { CoursesList } from "@/components/courses-list";
+import { getCourses } from "@/actions/get-courses";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const SearchPage = async () => {
+
+interface SearchPageProps {
+    searchParams: {
+        title: string;
+        categoryId: string;
+    }
+};
+
+const SearchPage = async ({
+    searchParams
+}: SearchPageProps) => {
+
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirect("/");
+    }
     
     const categories = await db.category.findMany({
         orderBy: {
             name: "asc"
         }
+    });
+
+    const courses = await getCourses({
+        userId,
+        ...searchParams,
     });
     
     return(
@@ -15,9 +40,12 @@ const SearchPage = async () => {
             <div className="px-6 pt-6 md:hidden md:mb-0 block">
                 <SearchInput />
             </div>
-            <div className="p-6">
+            <div className="p-6 space-y-4">
                 <Categories
                     items={categories}
+                />
+                <CoursesList
+                    items={courses}
                 />
             </div>
         </>
